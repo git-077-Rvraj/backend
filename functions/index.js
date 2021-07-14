0004d94bcab37abbfe3dfc3c13f91589f2c6dd93
Rv0777 <rvraj.0777@gmail.com>
2021-07-14 21:47:47 +0530

    done

--- functions/index.js
+++ functions/index.js
@@ -1,39 +1,80 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const PaytmChecksum = require("./PaytmChecksum");
+const https = require("https");
 
 admin.initializeApp();
 
-exports.getCheckSum = functions.https.onRequest((request, response) => {
-    var paytmParams = {};
-const CHECKSUM_VALUE = "";
-paytmParams["MID"] = "wRkmOe59237351879270";
-paytmParams["ORDERID"] = request.query.oId;
-paytmParams["WEBSITE"] = "WEBSTAGING";
-paytmParams["CHANNEL_ID"] = "WAP";
-paytmParams["INDUSTRY_TYPE_ID"] = "Retail";
-paytmParams["TXN_AMOUNT"]=request.query.amount;
-paytmParams["CALLBACK_URL"]= "https://securegw-stage.paytm.in/theia/paytmCallback?ORDER_ID="+request.query.oId;
+exports.getCheckSum = functions.https.onRequest((request, res) => {
+  var paytmParams = {};
+  console.log(request.query.oId);
 
-var paytmChecksum = PaytmChecksum.generateSignature(paytmParams, "Jf&eno32UcBVZ@ZY");
-paytmChecksum.then(function(result){
-	console.log("generateSignature Returns: " + result);
-    CHECKSUM_VALUE = result;
-	var verifyChecksum =  PaytmChecksum.verifySignature(paytmParams, "Jf&eno32UcBVZ@ZY",result);
-	console.log("verifySignature Returns: " + verifyChecksum);
-    response.json({
-        result:result,
+  paytmParams.body = {
+    requestType: "Payment",
+    mid: "wRkmOe59237351879270",
+    websiteName: "WEBSTAGING",
+    orderId: request.query.oId,
+    callbackUrl: "https://securegw-stage.paytm.in/theia/paytmCallback?ORDER_ID="+request.query.oId,
+    txnAmount: {
+      value: request.query.amount,
+      currency: "INR",
+    },
+    userInfo: {
+      custId: request.query.custId,
+    },
+
+    enablePaymentMode:[{
+      mode: "UPI",
+      channels:["UPIPUSH","UPIPUSHEXPRESS","UPI"]
+    },{
+       mode : "CREDIT_CARD",
+       channels:["VISA","MASTER","AMEX"]
+    },{
+      mode:"DEBIT_CARD",
+      channels:["VISA","MASTER","AMEX"],
+    }],
+  
+  };
+
+  PaytmChecksum.generateSignature(
+    JSON.stringify(paytmParams.body),
+    "Jf&eno32UcBVZ@ZY"
+  )
+    .then((checksum) => {
+      paytmParams.head = {
+        signature: checksum,
+      };
+
+      var post_data = JSON.stringify(paytmParams);
+
+      var options = {
+        /* for Staging */
+        hostname: "securegw-stage.paytm.in",
+        /* for Production */
+        // hostname: 'securegw.paytm.in',
+        port: 443,
+        path: `/theia/api/v1/initiateTransaction?mid=wRkmOe59237351879270&orderId=${request.query.oId}`,
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Content-Length": post_data.length,
+        },
+      };
+
+      var response = "";
+      var post_req = https.request(options, (post_res) => {
+        post_res.on("data", (chunk) => {
+          response += chunk;
+        });
+        post_res.on("end", () => {
+          console.log("Response: ", response);
+          res.send(response);
+        });
+      });
+      post_req.write(post_data);
+      post_req.end();
     })
-}).catch(function(error){
-	console.log(error);
-});
-
-const  paytmChecksum = CHECKSUM_VALUE;
-
-const isVerifySignature = PaytmChecksum.verifySignature(body, config.PAYTM_MERCHANT_KEY, paytmChecksum);
-if (isVerifySignature) {
-	console.log("Checksum Matched");
-} else {
-	console.log("Checksum Mismatched");
-}
-});
+    .catch((err) => {
+      console.error(err);
+    });
+}); 
